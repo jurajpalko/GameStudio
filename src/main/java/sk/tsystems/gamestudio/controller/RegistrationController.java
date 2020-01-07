2142235d8c53376377ccb3d18830c9a02ca9fa04
student <student@DESKTOP-5TNS856>
2020-01-07 15:45:56 +0100

    registration evaluation

--- src/main/java/sk/tsystems/gamestudio/controller/RegistrationController.java
+++ src/main/java/sk/tsystems/gamestudio/controller/RegistrationController.java
@@ -17,7 +17,8 @@ import sk.tsystems.gamestudio.service.PlayerService;
 public class RegistrationController {
 	private Player loggedPlayer;
 	private String name;
-	private String registrationErr = "";
+	private String nameErr = "";
+	private String passwordErr = "";
 
 	@Autowired
 	PlayerService playerService;
@@ -27,36 +28,72 @@ public class RegistrationController {
 
 	@RequestMapping("/registration")
 	public String index() {
-		registrationErr = "";
+		nameErr = "";
+		passwordErr = "";
 		return "registration";
 	}
+
 	@RequestMapping("/notRegistered")
 	public String nReg() {
-		
+
 		return "registration";
 	}
-	
+
 	@RequestMapping("/registration/register")
 	public String register(Player player) {
-		if (player.getName().trim().length() > 0) {
-			Player nameUnvailable = playerService.getPlayer(player.getName());
-			try {
-				if (nameUnvailable == null) {
 
-					playerService.addPlayer(new Player(player.getName(), player.getPasswd()));
-					mainController.login(player);
+		if (player.getName().toLowerCase().trim().length() > 0) {
 
+			if (player.getName().trim().length() < 4 || player.getName().trim().length() > 12
+					|| player.getPasswd().length() < 4 || player.getPasswd().length() > 12
+					|| player.getName().contains(" ") || player.getPasswd().contains(" ")) {
+
+				if (player.getName().trim().length() < 4) {
+					nameErr = "Name too short!";
+
+				} else if (player.getName().trim().length() > 12) {
+					nameErr = "Name too long!";
+
+				} else if (player.getName().contains(" ")) {
+					nameErr = "Name can not contain spaces";
 				} else {
-					registrationErr="Name not available!";
+					nameErr = "";
+				}
+
+				if (player.getPasswd().length() < 4) {
+					passwordErr = "Password too short!";
+
+				} else if (player.getPasswd().length() > 12) {
+					passwordErr = "Password too long!";
+
+				} else if (player.getPasswd().contains(" ")) {
+					passwordErr = "Password can not contain spaces";
+				} else {
+					passwordErr = "";
+				}
+				return "redirect:/notRegistered";
+			} else {
+				passwordErr = "";
+				nameErr = "";
+				Player nameUnvailable = playerService.getPlayer(player.getName().toLowerCase());
+				try {
+					if (nameUnvailable == null) {
+
+						playerService.addPlayer(new Player(player.getName().toLowerCase(), player.getPasswd()));
+						mainController.login(player);
+
+					} else {
+						nameErr = "Name not available!";
+						return "redirect:/notRegistered";
+					}
+				} catch (Exception e) {
+					nameErr = "Registration not successful!";
+
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					System.out.println("Not able to register.");
 					return "redirect:/notRegistered";
 				}
-			} catch (Exception e) {
-				registrationErr="Registration not successful!";
-				
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				System.out.println("Not able to register.");
-				return "redirect:/notRegistered";
 			}
 		}
 
@@ -64,8 +101,12 @@ public class RegistrationController {
 
 	}
 
-	public String getRegistrationErr() {
-		return registrationErr;
+	public String getNameErr() {
+		return nameErr;
+	}
+
+	public String getPasswordErr() {
+		return passwordErr;
 	}
 
 }
